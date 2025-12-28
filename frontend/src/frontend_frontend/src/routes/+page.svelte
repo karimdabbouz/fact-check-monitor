1b8a856146a12f9086f462e0b36a2acf0198f471
karim <hey@karim.ooo>
2025-12-28 11:24:40 +0100

    add IC frontend

---
+++ frontend/src/frontend_frontend/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import "../index.scss";
+  import { backend } from "$lib/canisters";
+
+  let greeting = $state("")
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    backend.greet(name).then((response) => {
+      greeting = response;
+    });
+    return false;
+  }
+</script>
+
+<main>
+  <img src="/logo2.svg" alt="DFINITY logo" />
+  <br />
+  <br />
+  <form action="#" onsubmit={onSubmit}>
+    <label for="name">Enter your name: &nbsp;</label>
+    <input id="name" alt="Name" type="text" />
+    <button type="submit">Click Me!</button>
+  </form>
+  <section id="greeting">{greeting}</section>
+</main>
